8cb306163c0c9662b53a52b4258055ad1841f78a
Krystian Checinski <kchecinski@doz.pl>
2025-06-18 12:26:05 +0200

    Removed unnecessary classes

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/network/server/model/Links.kt
+++
@@ -1,9 +0,0 @@
-package pl.cuyer.domain.models.server.battlemetrics
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Links(
-    val prev: String?,
-    val next: String?
-)
