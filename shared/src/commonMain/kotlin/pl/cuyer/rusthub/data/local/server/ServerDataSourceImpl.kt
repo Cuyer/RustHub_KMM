1066310a6d9626c1b4bdff189fd292e11b1c9000
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-06-20 13:04:46 +0200

    Update server queries to use stored filters

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/local/server/ServerDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/local/server/ServerDataSourceImpl.kt
@@ -5,10 +5,10 @@ import app.cash.sqldelight.paging3.QueryPagingSource
 import database.ServerEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import pl.cuyer.rusthub.common.Constants.DEFAULT_KEY
 import pl.cuyer.rusthub.data.local.Queries
 import pl.cuyer.rusthub.data.local.mapper.toEntity
 import pl.cuyer.rusthub.database.RustHubDatabase
-import pl.cuyer.rusthub.domain.model.Order
 import pl.cuyer.rusthub.domain.model.ServerInfo
 import pl.cuyer.rusthub.domain.model.ServerQuery
 import pl.cuyer.rusthub.domain.repository.server.ServerDataSource
@@ -49,36 +49,14 @@ class ServerDataSourceImpl(
 
     override fun getServersPagingSource(query: ServerQuery?): PagingSource<Int, ServerEntity> {
         val pagingSource: PagingSource<Int, ServerEntity> = QueryPagingSource(
-            countQuery = queries.countPagedServersFiltered(
-                wipe = query?.wipe?.toString(),
-                ranking = query?.ranking,
-                modded = if (query?.modded == true) 1 else null,
-                player_count = query?.playerCount,
-                map_name = query?.map?.toEntity(),
-                server_flag = query?.flag?.toEntity(),
-                region = query?.region?.toEntity(),
-                group_limit = query?.groupLimit,
-                difficulty = query?.difficulty?.toEntity(),
-                wipe_schedule = query?.wipeSchedule?.toEntity(),
-                is_official = if (query?.official == true) 1 else null
-            ),
+            countQuery = queries.countPagedServersFiltered(id = DEFAULT_KEY),
             transacter = queries,
             context = Dispatchers.IO,
             queryProvider = { limit: Long, offset: Long ->
                 queries.findServersPagedFiltered(
+                    id = DEFAULT_KEY,
                     limit = limit,
-                    offset = offset,
-                    ranking = query?.ranking,
-                    modded = if (query?.modded == true) 1 else null,
-                    player_count = query?.playerCount,
-                    map_name = query?.map?.toEntity(),
-                    server_flag = query?.flag?.toEntity(),
-                    region = query?.region?.toEntity(),
-                    group_limit = query?.groupLimit,
-                    difficulty = query?.difficulty?.toEntity(),
-                    wipe_schedule = query?.wipeSchedule?.toEntity(),
-                    is_official = if (query?.official == true) 1 else null,
-                    order = query?.order?.name ?: Order.WIPE.name
+                    offset = offset
                 )
             }
         )
