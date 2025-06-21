d9c66e8a9a02070c9a37d743041652871766d649
Krystian Checinski <krystixn.checinski@gmail.com>
2025-06-21 13:34:48 +0200

    Refactor: Use Koin definition DSL for ViewModel
    This commit updates the Koin module definition in `KoinInitializer.android.kt` to use `factoryOf` for ViewModel creation instead of the deprecated `viewModel` DSL.
    
    This aligns with the latest Koin practices and improves code clarity.

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/presentation/di/KoinInitializer.android.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/presentation/di/KoinInitializer.android.kt
@@ -1,15 +1,15 @@
 package pl.cuyer.rusthub.presentation.di
 
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import pl.cuyer.rusthub.data.local.DatabaseDriverFactory
 import pl.cuyer.rusthub.data.network.HttpClientFactory
 import pl.cuyer.rusthub.database.RustHubDatabase
-import pl.cuyer.rusthub.util.ClipboardHandler
 import pl.cuyer.rusthub.presentation.features.ServerDetailsViewModel
 import pl.cuyer.rusthub.presentation.features.ServerViewModel
+import pl.cuyer.rusthub.util.ClipboardHandler
 
 actual val platformModule: Module = module {
     single<RustHubDatabase> { DatabaseDriverFactory(androidContext()).create() }
