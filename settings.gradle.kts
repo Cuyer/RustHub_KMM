8cfa4623e41a8f55389d1db17dca3795ac8a36ac
Krystian Checinski <krystixn.checinski@gmail.com>
2025-06-23 01:00:24 +0200

    Refactor: Update dependencies and Android SDK versions
    This commit updates several dependencies and configuration settings:
    
    - **Android SDK:**
        - `android-minSdk` increased from 26 to 30.
    - **Dependencies:**
        - Ktor updated from `3.1.3` to `3.2.0`.
        - Navigation updated from `1.0.0-alpha01` to `1.0.0-alpha04`.
        - Navigation ViewModel updated from `1.0.0-alpha01` to `1.0.0-alpha02`.
        - Activity Compose updated from `1.12.0-alpha01` to `1.12.0-alpha03`.
        - Material3 Adaptive Navigation Suite updated from `1.3.2` to `1.4.0-alpha16`.
    - **Repository:**
        - Removed snapshot version variable and updated the AndroidX KMP snapshot repository URL.
    - **Compose:**
        - Removed `compose-runtime` as an explicit dependency.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,23 @@
-val snapshotVersion : String? = "13508953"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven {
+            url = uri("https://androidx.dev/kmp/builds/13678680/artifacts/snapshots/repository")
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        snapshotVersion?.let {
-            println("https://androidx.dev/snapshots/builds/$it/artifacts/repository/")
-            maven { url = uri("https://androidx.dev/snapshots/builds/$it/artifacts/repository/") }
-        }
-
         google()
         mavenCentral()
+        maven {
+            url = uri("https://androidx.dev/kmp/builds/13678680/artifacts/snapshots/repository")
+        }
     }
 }
 
