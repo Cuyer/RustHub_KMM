898c3698fcc81b20175d9b101371937a65da9098
Krystian Checinski <krystixn.checinski@gmail.com>
2025-06-25 23:18:46 +0200

    Refactor: Add onNewToken override to FirebaseMessagingService
    This commit adds an override for the `onNewToken` function in `RustHubFirebaseMessagingService`.
    The overridden function currently calls `super.onNewToken(token)`.

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/notification/RustHubFirebaseMessagingService.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/notification/RustHubFirebaseMessagingService.kt
@@ -2,8 +2,8 @@ package pl.cuyer.rusthub.notification
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import pl.cuyer.rusthub.util.NotificationPresenter
 import pl.cuyer.rusthub.domain.model.NotificationType
+import pl.cuyer.rusthub.util.NotificationPresenter
 
 class RustHubFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
@@ -12,5 +12,9 @@ class RustHubFirebaseMessagingService : FirebaseMessagingService() {
         } ?: return
         val id = message.data["id"] ?: return
         NotificationPresenter(this).show(id, type)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
     }
 }
