3cee15f7cc328fe329d4898ec079b9d5d6013e6d
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-06-29 00:03:33 +0200

    Feat: register FCM token

--- shared/src/iosMain/kotlin/pl/cuyer/rusthub/util/MessagingTokenManager.ios.kt
+++ shared/src/iosMain/kotlin/pl/cuyer/rusthub/util/MessagingTokenManager.ios.kt
@@ -6,6 +6,7 @@ actual class MessagingTokenManager(
     private val repository: MessagingTokenRepository,
     private val scheduler: MessagingTokenScheduler
 ) {
+    actual suspend fun registerToken(token: String) {}
     actual suspend fun currentToken(): String? = null
     actual suspend fun deleteToken() {}
 }
