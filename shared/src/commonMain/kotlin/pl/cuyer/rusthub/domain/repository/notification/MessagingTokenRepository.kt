7a3b58c7e4efe3c4269982416c4deda2bad5d5a4
Krystian Checinski <krystixn.checinski@gmail.com>
2025-06-30 17:07:47 +0200

    fix

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/repository/notification/MessagingTokenRepository.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/repository/notification/MessagingTokenRepository.kt
@@ -2,5 +2,4 @@ package pl.cuyer.rusthub.domain.repository.notification
 
 interface MessagingTokenRepository {
     suspend fun registerToken(token: String)
-    suspend fun deleteToken(token: String)
 }
