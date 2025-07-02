3080596a6a82903fa15bf2285558da7bf8a6cceb
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-02 22:32:08 +0200

    Work on settings

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/TokenRefresher.android.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/TokenRefresher.android.kt
@@ -1,12 +1,12 @@
 package pl.cuyer.rusthub.util
 
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.authProviders
 import io.ktor.client.plugins.auth.providers.BearerAuthProvider
 
-actual class TokenRefresher(private val httpClient: HttpClient) {
+actual class TokenRefresher actual constructor(private val httpClient: HttpClient) {
     actual fun clear() {
-        httpClient.plugin(Auth).providers
+        httpClient.authProviders
             .filterIsInstance<BearerAuthProvider>()
             .forEach { it.clearToken() }
     }
