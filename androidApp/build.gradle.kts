a6db85c57eaa2bf00cb71ace87f62d25d2927589
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-11 23:17:52 +0200

    fix pipeline

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -49,9 +49,9 @@ android {
     signingConfigs {
         create("development") {
             storeFile = rootProject.file("keystore-dev.jks")
-            storePassword = System.getenv("DEV_STORE_PASSWORD")
-            keyAlias = System.getenv("DEV_SIGNING_KEY_ALIAS")
-            keyPassword = System.getenv("DEV_SIGNING_KEY_PASSWORD")
+            storePassword = System.getenv("DEV_STORE_PASSWORD") ?: "2f032facZ@"
+            keyAlias = System.getenv("DEV_SIGNING_KEY_ALIAS") ?: "androiddev"
+            keyPassword = System.getenv("DEV_SIGNING_KEY_PASSWORD") ?: "2f032facZ@"
         }
         create("production") {
             storeFile = rootProject.file("keystore-prod.jks")
