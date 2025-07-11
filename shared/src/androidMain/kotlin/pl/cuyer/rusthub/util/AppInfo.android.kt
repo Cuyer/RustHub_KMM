edef668bb46d7f5e1f337331530c2fd3b92b62c7
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-11 22:19:57 +0200

    fix pipeline

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/AppInfo.android.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/AppInfo.android.kt
@@ -1,6 +1,6 @@
 package pl.cuyer.rusthub.util
 
-import pl.cuyer.rusthub.android.BuildConfig
+import pl.cuyer.rusthub.BuildConfig
 
 actual object AppInfo {
     actual val versionName: String = BuildConfig.VERSION_NAME
