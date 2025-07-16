1b99305c6956056c763795674cceea646df7d066
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-07-17 00:50:12 +0200

    Add: default locale file for per-app language

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/presentation/features/settings/SettingsViewModel.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/presentation/features/settings/SettingsViewModel.kt
@@ -42,6 +42,7 @@ import pl.cuyer.rusthub.util.GoogleAuthClient
 import pl.cuyer.rusthub.util.anonymousAccountExpiresIn
 import pl.cuyer.rusthub.util.formatExpiration
 import pl.cuyer.rusthub.util.StringProvider
+import pl.cuyer.rusthub.util.updateAppLanguage
 
 class SettingsViewModel(
     private val getSettingsUseCase: GetSettingsUseCase,
@@ -93,6 +94,7 @@ class SettingsViewModel(
 
     private fun updateLanguage(language: Language) {
         _state.update { it.copy(language = language) }
+        updateAppLanguage(language)
         save()
     }
 
