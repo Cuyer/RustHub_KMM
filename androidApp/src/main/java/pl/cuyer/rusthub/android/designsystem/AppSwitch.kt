dabc475944af4b45f412ed87cd704af04d6a0711
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-07-19 20:49:59 +0200

    Fix: improve switch accessibility

--- androidApp/src/main/java/pl/cuyer/rusthub/android/designsystem/AppSwitch.kt
+++ androidApp/src/main/java/pl/cuyer/rusthub/android/designsystem/AppSwitch.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,9 +12,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.stateDescription
 import pl.cuyer.rusthub.SharedRes
 import pl.cuyer.rusthub.android.util.composeUtil.stringResource
 
@@ -25,9 +25,15 @@ fun AppSwitch(
     onCheckedChange: (Boolean) -> Unit
 ) {
     var checked by remember(isChecked) { mutableStateOf(isChecked) }
-    val context = LocalContext.current
     Switch(
-        modifier = Modifier.semantics { contentDescription = SharedRes.strings.demo_with_icon.getString(context) },
+        modifier = Modifier.semantics {
+            role = Role.Switch
+            stateDescription = if (checked) {
+                stringResource(SharedRes.strings.enabled)
+            } else {
+                stringResource(SharedRes.strings.disabled)
+            }
+        },
         checked = checked,
         onCheckedChange = {
             checked = it
