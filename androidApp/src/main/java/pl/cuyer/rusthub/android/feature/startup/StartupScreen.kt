79cacb085f8a4a1bfa59dd5bdfe0cb878fd79272
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-20 18:19:48 +0200

    Fixes

--- androidApp/src/main/java/pl/cuyer/rusthub/android/feature/startup/StartupScreen.kt
+++ androidApp/src/main/java/pl/cuyer/rusthub/android/feature/startup/StartupScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import pl.cuyer.rusthub.SharedRes
 import pl.cuyer.rusthub.android.theme.spacing
 import pl.cuyer.rusthub.android.util.composeUtil.stringResource
@@ -27,11 +32,12 @@ fun StartupScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(spacing.medium)
         ) {
-            Image(
-                painter = painterResource(id = getImageByFileName("rusthub_logo").drawableResId),
-                contentDescription = stringResource(SharedRes.strings.application_logo)
+            CircularWavyProgressIndicator()
+            Text(
+                text = "Synchronizing data",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold
             )
-            LoadingIndicator()
         }
     }
 }
