982637bf870664e488dc9a018a337288ca1dc032
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-07-22 22:02:58 +0200

    Enhance baseline navigation

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -90,6 +90,11 @@ android {
     }
 }
 
+baselineProfile {
+    // Merge profile into the main app to enable optimizations on release builds.
+    mergeIntoMain = true
+}
+
 dependencies {
     implementation(projects.shared)
     implementation(project.dependencies.platform(libs.compose.bom))
