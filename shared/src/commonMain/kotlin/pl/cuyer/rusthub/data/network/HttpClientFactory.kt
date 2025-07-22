14dfc641a95b33f574ba2f0fcf988c93199eeb43
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-22 21:30:12 +0200

    Fixes

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/network/HttpClientFactory.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/network/HttpClientFactory.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import kotlinx.serialization.json.Json
 import pl.cuyer.rusthub.domain.repository.auth.AuthDataSource
 import pl.cuyer.rusthub.util.AppCheckTokenProvider
+import pl.cuyer.rusthub.util.TokenRefresher
 
 expect class HttpClientFactory(
     json: Json,
