6e55a44da40c0b74692dcb21ef962a328782bd0b
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-26 23:03:57 +0200

    Fixes

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/presentation/model/SubscriptionPlan.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/presentation/model/SubscriptionPlan.kt
@@ -4,7 +4,7 @@ import dev.icerock.moko.resources.StringResource
 import pl.cuyer.rusthub.SharedRes
 
 enum class SubscriptionPlan(val productId: String, val label: StringResource, val billed: StringResource) {
-    MONTHLY("pro_monthly", SharedRes.strings.monthly, SharedRes.strings.billed_monthly),
-    YEARLY("pro_yearly", SharedRes.strings.yearly, SharedRes.strings.billed_yearly),
-    LIFETIME("pro_lifetime", SharedRes.strings.lifetime, SharedRes.strings.pay_once)
+    MONTHLY("pro-monthly", SharedRes.strings.monthly, SharedRes.strings.billed_monthly),
+    YEARLY("pro-yearly", SharedRes.strings.yearly, SharedRes.strings.billed_yearly),
+    LIFETIME("pro-lifetime", SharedRes.strings.lifetime, SharedRes.strings.pay_once)
 }
