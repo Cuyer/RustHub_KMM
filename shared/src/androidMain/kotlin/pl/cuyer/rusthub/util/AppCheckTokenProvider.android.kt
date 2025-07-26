6a4ae686a0c1e28df4bea821768580e23fbf3421
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-07-26 09:00:48 +0200

    Enhance App Check logging

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/AppCheckTokenProvider.android.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/util/AppCheckTokenProvider.android.kt
@@ -2,15 +2,23 @@ package pl.cuyer.rusthub.util
 
 import com.google.firebase.appcheck.FirebaseAppCheck
 import io.github.aakira.napier.Napier
+import pl.cuyer.rusthub.util.CrashReporter
 import kotlinx.coroutines.tasks.await
 
 actual class AppCheckTokenProvider actual constructor() {
 
     actual suspend fun currentToken(): String? {
+        CrashReporter.log("Requesting AppCheck token")
         return try {
-            FirebaseAppCheck.getInstance().getToken(false).await().token
+            Napier.d("Requesting AppCheck token", tag = "AppCheck")
+            val token = FirebaseAppCheck.getInstance().getToken(false).await().token
+            CrashReporter.log("AppCheck token acquired")
+            Napier.d("AppCheck token acquired", tag = "AppCheck")
+            token
         } catch (e: Exception) {
-            Napier.e("Failed to get AppCheck token", e)
+            CrashReporter.recordException(e)
+            CrashReporter.log("Failed to get AppCheck token: ${e.message}")
+            Napier.e(message = "Failed to get AppCheck token", throwable = e, tag = "AppCheck")
             null
         }
     }
