1fba5139db1655c7b5e301033378a3f09d90a1b4
Krystian Checinski <krystixn.checinski@gmail.com>
2025-07-28 21:52:13 +0200

    Fixes

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/model/UserStatus.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/model/UserStatus.kt
@@ -1,8 +1,10 @@
 package pl.cuyer.rusthub.domain.model
 
 import androidx.compose.runtime.Immutable
+import kotlinx.serialization.Serializable
 
 @Immutable
+@Serializable
 data class UserStatus(
     val emailConfirmed: Boolean,
     val subscribed: Boolean,
