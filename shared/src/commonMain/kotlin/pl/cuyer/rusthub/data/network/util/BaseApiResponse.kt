b0129f2f478d6873abedcbd3770bbfdc83847b69
Krystian Checinski <krystixn.checinski@gmail.com>
2025-08-03 13:42:34 +0200

    Fix

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/network/util/BaseApiResponse.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/data/network/util/BaseApiResponse.kt
@@ -1,12 +1,11 @@
 package pl.cuyer.rusthub.data.network.util
 
-import io.github.aakira.napier.Napier
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
@@ -14,8 +13,6 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import pl.cuyer.rusthub.common.Result
 import pl.cuyer.rusthub.data.network.model.ErrorResponse
-import pl.cuyer.rusthub.data.network.util.ApiExceptionMapper
-import pl.cuyer.rusthub.domain.exception.ServiceUnavailableException
 import pl.cuyer.rusthub.util.CrashReporter
 import kotlin.coroutines.coroutineContext
 
