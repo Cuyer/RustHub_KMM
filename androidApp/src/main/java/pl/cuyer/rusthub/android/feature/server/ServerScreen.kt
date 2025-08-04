e125950b303f2b068ee82e38d0ebfe122e84551e
Krystian Checinski <krystixn.checinski@gmail.com>
2025-08-04 23:11:21 +0200

    Fix

--- androidApp/src/main/java/pl/cuyer/rusthub/android/feature/server/ServerScreen.kt
+++ androidApp/src/main/java/pl/cuyer/rusthub/android/feature/server/ServerScreen.kt
@@ -398,20 +398,6 @@ fun ServerScreen(
                             isOnline = item.serverStatus == ServerStatus.ONLINE
                         )
                     }
-                    onAppendItem {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            CircularWavyProgressIndicator(
-                                Modifier
-                                    .animateItem()
-                                    .padding(6.dp)
-                            )
-                        }
-                    }
                 }
             }
             AnimatedVisibility(
