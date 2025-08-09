bdf3f56638a1104e23b8d7cf5b558f028c48cf0a
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2025-08-09 10:11:12 +0200

    Refactor: remove unnecessary coroutine scope

--- shared/src/androidMain/kotlin/pl/cuyer/rusthub/work/ItemsWorker.kt
+++ shared/src/androidMain/kotlin/pl/cuyer/rusthub/work/ItemsWorker.kt
@@ -3,7 +3,6 @@ package pl.cuyer.rusthub.work
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import kotlinx.coroutines.flow.collectLatest
 import pl.cuyer.rusthub.domain.repository.item.ItemRepository
 import pl.cuyer.rusthub.domain.repository.item.local.ItemDataSource
 import pl.cuyer.rusthub.domain.repository.item.local.ItemSyncDataSource
@@ -19,24 +18,14 @@ class ItemsWorker(
 ) : CoroutineWorker(appContext, params) {
 
     override suspend fun doWork(): Result {
-        var hadError = false
-        repository.getItems().collect { result ->
-            when (result) {
-                is DomainResult.Success -> {
-                    dataSource.upsertItems(result.data)
-                    syncDataSource.setState(ItemSyncState.PENDING)
-                }
-                is DomainResult.Error -> {
-                    hadError = true
-                }
+        syncDataSource.setState(ItemSyncState.PENDING)
+        return when (val result = repository.getItems()) {
+            is DomainResult.Success -> {
+                dataSource.upsertItems(result.data)
+                syncDataSource.setState(ItemSyncState.DONE)
+                Result.success()
             }
-        }
-
-        return if (!hadError) {
-            syncDataSource.setState(ItemSyncState.DONE)
-            Result.success()
-        } else {
-            Result.retry()
+            is DomainResult.Error -> Result.retry()
         }
     }
 }
--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/repository/item/ItemRepository.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/repository/item/ItemRepository.kt
@@ -1,9 +1,8 @@
 package pl.cuyer.rusthub.domain.repository.item
 
-import kotlinx.coroutines.flow.Flow
 import pl.cuyer.rusthub.common.Result
 import pl.cuyer.rusthub.domain.model.RustItem
 
 interface ItemRepository {
-    fun getItems(): Flow<Result<List<RustItem>>>
+    suspend fun getItems(): Result<List<RustItem>>
 }
