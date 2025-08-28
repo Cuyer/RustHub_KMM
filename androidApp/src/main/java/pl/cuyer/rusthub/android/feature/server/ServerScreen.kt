9c29ded7e826cc232d0d503c4e509b269d25d194
Krystian Checinski <kchecinski@doz.pl>
2025-08-28 15:39:28 +0200

    fix

--- androidApp/src/main/java/pl/cuyer/rusthub/android/feature/server/ServerScreen.kt
+++ androidApp/src/main/java/pl/cuyer/rusthub/android/feature/server/ServerScreen.kt
@@ -141,6 +141,12 @@ fun ServerScreen(
 
     val pullToRefreshState = rememberPullToRefreshState()
 
+    LaunchedEffect(pagedList.itemSnapshotList.items) {
+        if (pagedList.itemCount > 0) {
+            lazyListState.scrollToItem(0)
+            scrollBehavior.scrollOffset = 1f
+        }
+    }
 
     val isAtTop by remember {
         derivedStateOf {
