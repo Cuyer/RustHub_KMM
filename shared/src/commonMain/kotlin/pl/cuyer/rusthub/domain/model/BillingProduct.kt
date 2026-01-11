f1429190222d279ebdc2ae774587952d59f702f7
Krystian Chęciński <51329471+Cuyer@users.noreply.github.com>
2026-01-11 12:48:41 +0100

    Fix: show free trial offers in subscription UI

--- shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/model/BillingProduct.kt
+++ shared/src/commonMain/kotlin/pl/cuyer/rusthub/domain/model/BillingProduct.kt
@@ -7,5 +7,6 @@ data class BillingProduct(
     val id: String,
     val title: String,
     val description: String,
-    val price: String
+    val price: String,
+    val hasFreeTrial: Boolean = false
 )
